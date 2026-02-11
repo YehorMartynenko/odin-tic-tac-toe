42dc8ea6d88dffdf712a0c727cbc1dd650588cca
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-02-11 06:30:41 +0100

    Add win condition

--- js/main.js
+++ js/main.js
@@ -1,32 +1,43 @@
 const Gameboard = (function() {
     const size = 3;
-    const board = [];
+    
 
-    for(let i = 0; i<size; i++){
-        board[i] = [];
-        for(let j = 0; j<size; j++){
-            board[i].push(createCell());
+    const createBoard = () => {
+        const arr = [];
+        for(let i = 0; i<size; i++){
+            arr[i] = [];
+                for(let j = 0; j<size; j++){
+                    arr[i].push(createCell());
+                }
         }
-    };
+        return arr;
+    }
+
+    let board = createBoard();
+
+    const resetBoard = () => {
+        board = createBoard();
+    }
 
     const getBoard = () => board;
 
     const markCell = (row, column, playerMark) => {
         if(board[row][column].getValue() === ""){
             board[row][column].addMark(playerMark);
-            return "Success";
+            return true;
         } else {
             console.log("You have to pick empty cell");
-            return "Error";
+            return false;
         }
     };
 
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardWithCellValues);
+        return boardWithCellValues;
     };
 
-    return { getBoard, markCell, printBoard };
+    return { getBoard, markCell, printBoard, resetBoard };
 })();
 
 function createCell() {
@@ -57,6 +68,7 @@ const GameController = (function(playerOne = "Player One", playerTwo = "Player T
     ];
 
     let activePlayer = players[0];
+    let winner = null;
     
     const switchActivePlayer = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -66,18 +78,89 @@ const GameController = (function(playerOne = "Player One", playerTwo = "Player T
         return activePlayer;
     };
 
+    
+
     const printNewRound = () => {
         Gameboard.printBoard();
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
+    const newGame = () => {
+        Gameboard.printBoard();
+        Gameboard.resetBoard();
+        winner = null;
+        activePlayer = players[0];
+        printNewRound();
+    }
+
+    const checkWin = () => {
+        const board = Gameboard.printBoard();
+        
+        const getLines = (arr) => {
+            const lines = [];
+            //rows
+            for(let i = 0; i<arr.length; i++){
+                lines.push(arr[i]);
+            }
+
+            //columns 
+            const getColumn = (arr, index) => arr.map(el => el[index]);
+            for(let i = 0; i<arr.length; i++){
+                lines.push(getColumn(arr, i));
+            }
+            
+            //diagonals 
+            const diagonalOne = [];
+            const diagonalTwo = [];
+            for(let i = 0; i<arr.length; i++){
+                diagonalOne.push(arr[i][i]);
+                diagonalTwo.push(arr[i][arr.length-(i+1)]);
+            }
+            lines.push(diagonalOne);
+            lines.push(diagonalTwo)
+
+            return lines;
+        }
+
+        
+        const isEqual = (arrLine, index) => {
+            const element = arrLine[index][0];
+            console.log(`checking line ${arrLine[index]}, element ${element}`);
+            return arrLine[index].every(el => el === element && el != "");
+        }
+
+        const checkEquality = () => {
+            const arr = getLines(board);
+
+            for(let i = 0; i<arr.length; i++){
+                console.log(`Checking for equality ${arr[i]}`);
+                if(isEqual(arr, i)){
+                    console.log("true");
+                    return true;
+                } else {
+                    console.log("false");
+                }
+            }
+            return false;
+        }
+        return checkEquality();
+    }
+
+    
+
     const playRound = (row, column) => {
         console.log(`${getActivePlayer().name} puts ${getActivePlayer().mark} into row ${row}, column ${column}`);
-        if(Gameboard.markCell(row, column, getActivePlayer().mark) === "Error") {
+        if(!Gameboard.markCell(row, column, getActivePlayer().mark)) {
             printNewRound();
         } else {
-            switchActivePlayer();
-            printNewRound();
+            if(checkWin()){
+                winner = getActivePlayer();
+                console.log(`${winner.name} won`);
+                newGame();
+            } else {
+                switchActivePlayer();
+                printNewRound();
+            }
         }
     };
 
@@ -86,7 +169,8 @@ const GameController = (function(playerOne = "Player One", playerTwo = "Player T
     return {
         playRound,
         getActivePlayer,
-        getBoard: Gameboard.getBoard()
+        getBoard: Gameboard.getBoard,
+        newGame,
     }
     
 })();
